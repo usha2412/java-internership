32fb485f43bb25a7dbe9be56e2d86abe071a4393
usha2412 <24ushapal@gmail.com>
2025-09-26 12:10:54 +0530

    FileHandling

---
+++ src/NotesManage.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NotesManage {
+    public static void main (String[] args) throws IOException {
+        String  path ="src/JavaNotes.txt";
+        File myNotes = new File(path);
+        System.out.print("Enter your notes:-");
+        FileWriter myWriter = new FileWriter(myNotes,true);
+//        myWriter.write("Java is very powerful language." );
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        myWriter.write(text);
+        myWriter.close();
+        scanner.close();
+        System.out.println(" File writing is done");
+
+        FileReader fr =new FileReader(myNotes);
+        int i;
+        for( i=fr.read();i !=-1;i=fr.read())
+        System.out.print(((char)i));
+        fr.close();
+
+
+
+
+
+    }
+}
